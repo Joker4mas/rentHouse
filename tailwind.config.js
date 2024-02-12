68471cbb2a8d81d2d9e8708c4003cde1d38e2582
joker4mas <samuelogbaje@gmail.com>
2024-02-12 21:10:24 +0100

    fixed the hero-section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,10 @@ module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
     extend: {
-  
+      //font-family
+      fontFamily :{
+        'philo' : ['Philosopher, sans-serif'],
+      } 
     },
   },
   plugins: [],
